7bb1892fa8c0900427f884d92a7ca5b84f19f0c9
Tiffany <tar3820@gmail.com>
2015-12-08 21:05:43 -0800

    Updating dice things in casino.

--- casino/casino.js
+++ casino/casino.js
@@ -1,16 +1,24 @@
 
+document.getElementById("roll").onclick = roll;
+
+function roll() {
+   document.getElementById("dice").innerHTML = "";
+
+   var num = document.getElementById("num").value;
+
+   var dice_img = ['1.png', '2.png', '3.png', '4.png', '5.png', '6.png'];
+
+   for (var i = 0; i < num; i++) {
+      // Generate a random number between 1 and 6 
+      var side = Math.floor(Math.random() * 6) + 1;
+
+      // Show the correct dice side for the randomly generated number
+
+      // <img src='3.png' />
+      document.getElementById("dice").innerHTML += "<img src='" + dice_img[side-1] + "' />";
+   };
 
 
-var searchRequest = { "phrase": "dog" }
-
-
-function GetSearchResults(callback) {
-  $.ajax({
-    type: "GET",
-    url: "http://version1.api.memegenerator.net/Generators_Search?q=corgi&pageIndex=0&pageSize=12",
-    })
-    .success(function (data, textStatus, jqXHR) { 
-         document.getElementById("images").innerHTML = data;
-         console.log(data); })
-    .fail(function (data, err) { /* handle errors */ });
 }
+
+
